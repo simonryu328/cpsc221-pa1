d9584efdadd2d38f45ece3344a24de37b859e6c1
Simon Ryu <simonryu328@gmail.com>
2021-01-31 14:18:38 -0800

    height and width methods

--- block.cpp
+++ block.cpp
@@ -4,10 +4,12 @@
 
 int Block::height() const {
   /* your code here */
+  return data[0].size();
 }
 
 int Block::width() const {
   /* your code here */
+  return data.size();
 }
 
 void Block::render(PNG &im, int x) const {
